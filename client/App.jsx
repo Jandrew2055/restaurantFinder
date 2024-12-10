0fc8a11be00b5353efbddb2b55e0419eb050f64b
Jose Andrew <Jandrew205@outlook.com>
2024-12-10 01:52:09 -0500

    added support for react

---
+++ client/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  console.log('testing');
+  console.log(135);
+
+  return (
+    <div>
+      <h1>testing</h1>
+      <h2>testing number one</h2>
+    </div>
+  );
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -23,12 +23,12 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },
