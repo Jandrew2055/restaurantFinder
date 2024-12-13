2812bace0364d572feb5c79e8b4ea8b202fd036a
Jose Andrew <Jandrew205@outlook.com>
2024-12-13 14:42:23 -0500

    test

--- client/Components/chatBot.jsx
+++ client/Components/chatBot.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Chatbot = () => {
+  const handleSubmit = () => {};
+  const sendData = () => {};
+
   return (
     <div>
       <h1>AI CHATBOT</h1>
+      <form></form>
     </div>
   );
 };
