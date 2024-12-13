4a9dbf2e31d3a2f36d184df1f564fc07c3e1e54b
Jose Andrew <Jandrew205@outlook.com>
2024-12-13 15:15:47 -0500

    added file to gitignore

--- authentication.js
+++
@@ -1,4 +0,0 @@
-export const URL = 'https://api.yelp.com/v3/businesses/search';
-
-export const API_KEY =
-  'OFL5601OUDsphFX4AJDXtkQpSm0831DAILCRCezIK01q_vGR14ADCsRtz-yFtypULMKbBbSWQYo1YngUReWMdyF1m7lm5X-LHyWqO7OTzJgZ9hbsn5OVMoMsNZZYZ3Yx';
