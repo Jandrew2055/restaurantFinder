6b760475cf58e485286f60a975d324f5e380e8bb
Jose Andrew <Jandrew205@outlook.com>
2025-01-14 00:31:59 -0500

    added placeholder text for forum

--- client/Components/Forum.jsx
+++ client/Components/Forum.jsx
@@ -4,6 +4,12 @@ const Forum = () => {
   return (
     <div>
       <h1>This will be where the forum page lies.</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+        voluptates quia, ratione optio quod doloremque asperiores iste libero ad
+        inventore distinctio, nisi sunt maiores reprehenderit modi accusamus
+        reiciendis corporis sit?e
+      </p>
     </div>
   );
 };
