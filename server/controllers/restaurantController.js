9290a46229a5c7007e93d0378f75e48875f57ece
Jose Andrew <Jandrew205@outlook.com>
2025-05-02 10:56:42 -0400

    may not be experiencing 429 error after fixing the callback error, changed backend to do calls synchronously now

--- server/controllers/restaurantController.js
+++ server/controllers/restaurantController.js
@@ -80,38 +80,68 @@ restaurantController.getRestaurants = async (req, res, next) => {
 };
 
 restaurantController.getPhotos = async (req, res, next) => {
-  const { photoObject } = req.body; //grab object containing all restaurant ids and photoResource
-  let delay = 0;
+  const { photoObject } = req.body; // { restaurantId: photoResource }
 
-  //make a request to the API to grab the restaurant's photo
-  for (const [restaurantId, photoResource] of Object.entries(photoObject)) {
-    setTimeout(async () => {
-      const URL = `https://places.googleapis.com/v1/${photoResource}/media?maxHeightPx=400&maxWidthPx=400&key=${GOOGLE_API_KEY}&skipHttpRedirect=true`;
+  const result = {};
 
-      try {
-        const response = await fetch(URL);
-        if (!response.ok) {
-          throw new Error('Failed to fetch photo');
-        }
-        //parse response using .json()
-        const data = await response.json();
-        //update object now with photoURI in the value for each restaurant
-        photoObject[restaurantId] = data.photoUri;
-        // const photoUri = data.photoUri;
-      } catch (err) {
-        console.log('error fetching from api:', err);
-        // return next(err);
-      }
-    }, 0 + delay);
-    delay += 500;
-  }
+  const fetchPhoto = async (restaurantId, photoResource) => {
+    const url = `https://places.googleapis.com/v1/${photoResource}/media?maxHeightPx=400&maxWidthPx=400&key=${GOOGLE_API_KEY}&skipHttpRedirect=true`;
 
-  //ensures this gets calledd afterlast delay and not right away
-  setTimeout(() => {
-    // console.log('Testing photo object result:', photoObject);
-    res.locals.photos = photoObject;
-    return next();
-  }, delay);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) throw new Error('Failed to fetch photo');
+
+      const data = await response.json();
+      result[restaurantId] = data.photoUri;
+    } catch (err) {
+      console.log(`Error fetching photo for ${restaurantId}:`, err);
+      result[restaurantId] = null; // or set a placeholder image URI
+    }
+  };
+
+  // Use Promise.all to fetch photos in parallel
+  const fetchPromises = Object.entries(photoObject).map(([id, photoResource]) =>
+    fetchPhoto(id, photoResource)
+  );
+
+  await Promise.all(fetchPromises);
+
+  res.locals.photos = result;
+  return next();
+
+  //THIS WAS USING SET TIME OUT BELOW TO SPACE OUT CALLS TO PREVENT 429 ERROR, TESTING ABOVE AND MIGHT DELETE BELOW IF NOT NEEDED
+  // const { photoObject } = req.body; //grab object containing all restaurant ids and photoResource
+  // let delay = 0;
+
+  // //make a request to the API to grab the restaurant's photo
+  // for (const [restaurantId, photoResource] of Object.entries(photoObject)) {
+  //   setTimeout(async () => {
+  //     const URL = `https://places.googleapis.com/v1/${photoResource}/media?maxHeightPx=400&maxWidthPx=400&key=${GOOGLE_API_KEY}&skipHttpRedirect=true`;
+
+  //     try {
+  //       const response = await fetch(URL);
+  //       if (!response.ok) {
+  //         throw new Error('Failed to fetch photo');
+  //       }
+  //       //parse response using .json()
+  //       const data = await response.json();
+  //       //update object now with photoURI in the value for each restaurant
+  //       photoObject[restaurantId] = data.photoUri;
+  //       // const photoUri = data.photoUri;
+  //     } catch (err) {
+  //       console.log('error fetching from api:', err);
+  //       // return next(err);
+  //     }
+  //   }, 0 + delay);
+  //   delay += 500;
+  // }
+
+  // //ensures this gets calledd afterlast delay and not right away
+  // setTimeout(() => {
+  //   // console.log('Testing photo object result:', photoObject);
+  //   res.locals.photos = photoObject;
+  //   return next();
+  // }, delay);
 };
 
 module.exports = restaurantController;
