85e97783493ba2b44ef3c0952fafb2da4258f1bc
Jose Andrew <Jandrew205@outlook.com>
2025-05-28 16:47:24 -0400

    setting up authProvider for tracking userSession and functions

--- client/Contexts/authContext.tsx
+++ client/Contexts/authContext.tsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import { useEffect, useState, useContext, createContext } from 'react';
+import supabase from '../Models/supabaseClient';
 
-const AuthContext = () => {};
+const AuthContext = createContext();
 
-export default AuthContext;
+const AuthProvider = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    //as soon as application opens up check if user active
+    const getUserSession = async () => {
+      const {
+        data: { session },
+        error,
+      } = await supabase.auth.getSession();
+
+      if (session?.user) {
+        setUser(session.user);
+      }
+    };
+
+    getUserSession();
+  }, []);
+};
+
+export default AuthProvider;
